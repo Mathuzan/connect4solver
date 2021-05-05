a09666d12f631fe8c3ee4a52f2c7c7259fd54225
igrek51 <igrek51.dev@gmail.com>
2021-05-06 00:46:32 +0200

    Update pull cache script

--- cache/pull.sh
+++ cache/pull.sh
@@ -2,6 +2,7 @@
 cd "$(dirname "$0")"
 
 # sudo apt install p7zip-full
-wget https://github.com/igrek51/connect4solver/raw/cache/cache/cache_7x6.protobuf.7z.001
+wget https://github.com/igrek51/connect4endgames/raw/cache/cache/cache_7x6.protobuf.7z.001
 7z x cache_7x6.protobuf.7z.001
+rm cache_7x6.protobuf.7z.001
 echo "Cached endgames ready"
--- cache/push.sh
+++ cache/push.sh
@@ -3,5 +3,6 @@ cd "$(dirname "$0")"
 
 # sudo apt install p7zip-full
 7z -v99m a cache_7x6.protobuf.7z cache_7x6.protobuf
+# copy to https://github.com/igrek51/connect4endgames
 # git checkout --orphan cache
-# git add cache_7x6.protobuf.7z.001
+# git add cache/cache_7x6.protobuf.7z.001
